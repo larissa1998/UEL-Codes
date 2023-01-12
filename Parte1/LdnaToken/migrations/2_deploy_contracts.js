f2a67526b749cc12421205dd2008e655cac9a56f
Larissa Fogaça <larissafogacaavare@hotmail.com>
2023-01-12 08:59:40 -0300

    ajuste comentário

--- Parte1/LdnaToken/migrations/2_deploy_contracts.js
+++ Parte1/LdnaToken/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var LdnaToken = artifacts.require("LdnaToken");
 
 module.exports = function(deployer) {
-  // Deploy the UELToken contract as our only task
+  // Deploy the LdnaToken contract as our only task
   deployer.deploy(LdnaToken);
 };
